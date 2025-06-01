85eeb4f1eb3553fe20387c59cd260eae609c3250
Hassan Rahhal <hassanrahhal09@gmail.com>
2025-06-01 17:39:42 +0200

    First update

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import "./App.scss";
+import Navbar from "components/navbar/navbar";
+import Store from "pages/store";
+import Cart from "pages/cart";
+import Authenticate from "pages/authenticate";
+import { Routes, Route } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, database } from "utils/firebaseConfig";
+import { MainContext } from "utils/context";
+import { useEffect, useState } from "react";
+import { fetchUserData, setupDBListener } from "utils/firebaseFunctions";
+import { products } from "utils/products";
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, loading] = useAuthState(auth);
+  const [cartProducts, setCartProducts] = useState();
+  const [username, setUsername] = useState();
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  useEffect(() => {
+    user && fetchUser();
+  }, [user]);
+
+  useEffect(() => {
+    if (!loading && user) {
+      setupDBListener(user, (data) => {
+        const updatedProducts = products.filter((product) => {
+          return !data.some((cartProduct) => cartProduct.id === product.id);
+        });
+        setFilteredProducts(updatedProducts);
+        setCartProducts(data);
+      });
+    }
+  }, [loading, user]);
+  const fetchUser = async () => {
+    const res = await fetchUserData(user);
+    if (res.success) {
+      console.log(res);
+      setUsername(res.data.username);
+      setCartProducts(res.data.cartProducts);
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <MainContext.Provider
+        value={{ user, loading, cartProducts, username, filteredProducts }}
+      >
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Store />}></Route>
+          <Route path="/cart" element={<Cart />}></Route>
+          <Route path="/authenticate" element={<Authenticate />}></Route>
+        </Routes>
+      </MainContext.Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
